8526c46c0a67f1125b2b576460b2be24256154c6
NetworkingCode <networkingcode@gmail.com>
2024-06-13 22:08:35 -0400

    Manipulacion del DOM, innerHTML

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -11,15 +11,20 @@ function seleccionarMascotaJugador() {
     let inputHipodoge = document.getElementById('hipodoge').checked
     let inputCapipepo = document.getElementById('capipepo').checked
     let inputRatigueya = document.getElementById('ratigueya').checked
+
+    let spanMascotaJugador = document.getElementById('mascota-jugador')
     
     if (inputHipodoge) {
-        alert('Seleccionaste a Hipodoge')
+        // alert('Seleccionaste a Hipodoge')
+        spanMascotaJugador.innerHTML = 'Hipodoge'
         
     } else if (inputCapipepo) {
-        alert('Seleccionaste a Capipepo')
+        // alert('Seleccionaste a Capipepo')
+        spanMascotaJugador.innerHTML = 'Capipepo'
         
     } else if (inputRatigueya) {
-        alert('Seleccionaste a Ratigueya')
+        // alert('Seleccionaste a Ratigueya')
+        spanMascotaJugador.innerHTML = 'Ratigueya'
 
     } else alert('Debes seleccionar una mascota')   
 }
